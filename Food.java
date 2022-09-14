2ca4c715a027761cd96510144fe117005c1a1f98
tragicmilk7720 <drew.pranab7@gmail.com>
2022-09-13 21:52:53 -0600

    updated the food file

--- Food.java
+++ Food.java
@@ -1,3 +1,40 @@
 public class Food {
+    private String name;
+    private int calorie;
+    private int protien;
+    private int carb;
+    private int fat;
     
+    public Food(String name, 
+        int calorie,
+        int protien,
+        int carb,
+        int fat){
+
+            this.name = name;
+            this.calorie = calorie;
+            this.protien = protien;
+            this.carb = carb;
+            this.fat = fat;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public int getCalorie(){
+        return calorie;
+    }
+
+    public int getProtien(){
+        return protien;
+    }
+
+    public int getCarb(){
+        return carb;
+    }
+
+    public int getFat(){
+        return fat;
+    }
 }
