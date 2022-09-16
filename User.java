330dd15a798cfd9df4ac66dc7bdc6992d4e54d73
bisheshkhanal <khanalbishesh12@gmail.com>
2022-09-16 11:26:03 -0600

    Update User.java
    added the change methods to each property of the user and documented them

--- User.java
+++ User.java
@@ -1,4 +1,7 @@
 public class User {
+
+    //declaring and initializing some local variables for the user class
+
     String firstName;
     String lastName;
     int age;
@@ -6,6 +9,7 @@ public class User {
     GENDER gender;
     ActivityLevelEnum activityLevel;
     
+    //constructor of the user class
     public User(String firstname,
         String lastname, 
         int age,
@@ -20,8 +24,65 @@ public class User {
             this.activityLevel = activityLevel;
     }
 
+    //methods 
+    
+    /**
+     * 
+     * @param firstname
+     * @return true after changing the first name of the user
+     */
     public boolean changeFirstName(String firstname){
         this.firstName = firstname;
+        return true;
+    }
+
+    /**
+     * 
+     * @param lastname
+     * @return true after changing the last name of the user
+     */
+    public boolean changeLastName(String lastname){
+        this.lastName = lastname;
+        return true;
+    }
+
+    /**
+     * 
+     * @param age
+     * @return true after changing the age of the user
+     */
+    public boolean changeAge(int age){
+        this.age = age;
+        return true;
+    }
+
+    /**
+     * 
+     * @param height
+     * @return true after changing the height of the user
+     */
+    public boolean changeHeight(int height){
+        this.height = height;
+        return true;
+    }
+
+    /**
+     * 
+     * @param gender
+     * @return true after changing the gender of the user
+     */
+    public boolean changeGender(GENDER gender){
+        this.gender = gender;
+        return true;
+    }
+
+    /**
+     * 
+     * @param activityLevel
+     * @return true after changing the actitivity Level of the user
+     */
+    public boolean changeActivityLevel(ActivityLevelEnum activityLevel){
+        this.activityLevel = activityLevel;
         return true;
     }
 }
